623d2fe217aa096f4abbd1d2d45bf41b6b98ee63
Arpan <rockarpan31@gmail.com>
2024-05-13 23:53:59 +0545

    riI

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -15,7 +15,7 @@ export function NavbarSticky() {
       icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
-      name: "What's Up",
+      name: "What's up",
       link: "/",
       icon: (
         <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
